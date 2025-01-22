4dbfda066b34800a87ce78ca44d37803c1ef7479
rishitarana <102794708+rishitarana@users.noreply.github.com>
2025-01-22 21:37:59 +0530

    Time: 5 ms (25.41%), Space: 45.5 MB (42.13%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int sum=(n*(n+1)/2);
+        int exSum=0;
+
+        for(int i=0;i<n;i++){
+            exSum=exSum+nums[i];
+        }
+
+        return sum-exSum;
+    }
+}
