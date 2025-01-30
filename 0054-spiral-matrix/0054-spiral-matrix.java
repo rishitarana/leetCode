03dd39c5bf6ca39117a5a1e74ac28cba42fddf29
rishitarana <102794708+rishitarana@users.noreply.github.com>
2025-01-30 19:15:52 +0530

    Time: 0 ms (100%), Space: 41.8 MB (16.2%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,97 +1,55 @@
-// import java.util.ArrayList;
-// import java.util.List;
-
-// class Solution {
-//     public List<Integer> spiralOrder(int[][] matrix) {
-//         int m = matrix.length;
-//         if (m == 0) return new ArrayList<>(); // Handle empty matrix case
-//         int n = matrix[0].length;
-
-//         int top = 0, bottom = m - 1;
-//         int left = 0, right = n - 1;
-
-//         ArrayList<Integer> list = new ArrayList<>();
-
-//         while (top <= bottom && left <= right) {
-//             // Traverse from left to right along the top row
-//             for (int i = left; i <= right; i++) {
-//                 list.add(matrix[top][i]);
-//             }
-//             top++;
-
-//             // Traverse from top to bottom along the right column
-//             for (int i = top; i <= bottom; i++) {
-//                 list.add(matrix[i][right]);
-//             }
-//             right--;
-
-//             if (top <= bottom) {
-//                 // Traverse from right to left along the bottom row
-//                 for (int i = right; i >= left; i--) {
-//                     list.add(matrix[bottom][i]);
-//                 }
-//                 bottom--;
-//             }
-
-//             if (left <= right) {
-//                 // Traverse from bottom to top along the left column
-//                 for (int i = bottom; i >= top; i--) {
-//                     list.add(matrix[i][left]);
-//                 }
-//                 left++;
-//             }
-//         }
-
-//         return list;
-//     }
-// }
-
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
 
         int m=matrix.length;
         int n=matrix[0].length;
 
-        int top=0 , bottom = m-1;
-        int left=0 , right= n-1;
+        int left=0;
+        int right=n-1;
+        int upper=0;
+        int bottom = m-1;
+
+        while(left<=right && upper<=bottom){
+            for(int i=left;i<=right;i++){
+                list.add(matrix[upper][i]);
+            }
+            upper++;
+
+            for(int i=upper;i<=bottom;i++){
+                list.add(matrix[i][right]);
+            }
+
+            right--;
+
+            if(upper<=bottom){
+                for(int i=right;i>=left;i--){
+                list.add(matrix[bottom][i]);
+            }   
+
+            bottom--;
+
+            }
+
+            if(left<=right){
+            for(int i=bottom;i>=upper;i--){
+                list.add(matrix[i][left]);
+            }
+
+            left++;
+            }
+            
+
+
+
+        }
+
+        return list;
+
+
         
-       ArrayList<Integer> list=new ArrayList<>();
-
-       while(top<=bottom && left<=right){
-    
-       //right
-
-       for(int i=left;i<=right;i++){
-        list.add(matrix[top][i]);
-       }
-       top++;
-
-       for(int i=top;i<=bottom;i++){
-        list.add(matrix[i][right]);
-       }
-       right--;
-
-        if(top<=bottom){
-
-       for(int i=right;i>=left;i--){
-        list.add(matrix[bottom][i]);
-        
-       }
-       bottom--;
-    }
-
-    if(left<=right){
-
-       for(int i=bottom;i>=top;i--){
-        list.add(matrix[i][left]);
-       }
-       left++;
-
-       }
-    }
-    return list;
-
         
     }
 }
